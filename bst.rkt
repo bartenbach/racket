6a9c4e241410a4b3118c0d41dc40c59970ec8e83
Blake Bartenbach <blakebartenbach@gmail.com>
2020-03-14 14:45:44 -0500

    Finally got tail recursive leaf function to work

--- bst.rkt
+++ bst.rkt
@@ -1,16 +1,46 @@
 #lang racket
 
 (require racket/trace)
+(require rackunit)
 ;; binary search tree
 ;; '()
 ;; '(x () ())
 ;; '(4 (3 () ()) (5 () ()))
 ;;  (root (left tree) (right tree))
 
-;; functions get left subtree, get right subtree, get leaf, etc
-(define (left-subtree tree) (car (cdr tree)))
-(define (right-subtree tree) (car (cdr (cdr tree))))
-(define (isLeaf tree x))
+; returns the left subtree of a given tree
+(define (left-subtree tree)
+  (car (cdr tree)))
+
+; returns the right subtree of a given tree
+(define (right-subtree tree)
+  (car (cdr (cdr tree))))
+
+; returns the subtree of a tree starting with node 'x' and all children
+(define (get-subtree tree x)
+  (cond
+    ([and (null? (left-subtree tree)) (null? (right-subtree tree))] null)
+    ([= x (car (left-subtree tree))] x)
+    ([= x (car (right-subtree tree))] x)
+    (else (get-subtree (left-subtree tree) x) (get-subtree (right-subtree tree) x))))
+
+; Returns true if x in tree is a leaf - otherwise false
+(trace-define (isLeaf? tree x)
+  (cond
+    ; the tree is empty
+    ([or (empty? tree) (empty? (car tree))] #f)
+    ; the root is equal to x and both subtrees are null
+    ([and (= x (car tree)) (null? (left-subtree tree)) (null? (right-subtree tree))] #t)
+    ; the root is not equal to x but both subtrees are null
+    ([and (null? (left-subtree tree)) (null? (right-subtree tree))] #f)
+    ; subtrees are not null, recursively check subtrees
+    ([< x (car tree)] (isLeaf? (left-subtree tree) x))
+    ([> x (car tree)] (isLeaf? (right-subtree tree) x))
+    (else #f)))
+    ;(else (isLeaf? (left-subtree tree) x) (isLeaf? (right-subtree tree) x))))
+    ;([and (null? (left-subtree (get-subtree tree x))) (null? (right-subtree (get-subtree tree x)))] #t)
+    ;(else #f)))
+
 
 (define (bst-insert tree x)
   (cond
@@ -23,10 +53,10 @@
   (cond
     ([null? (car tree)] null)
     ([empty? (car tree)] null)
-    ([isLeaf? x] tree without x)
-    ([hasOneChild? x] relink x.parent to x.child)
-    ([hasTwoChildren? x] delete and relink to smallest node in right tree)
-    ([not(= x (car tree))]
+    ([isLeaf? tree x] tree) ; return tree without x
+    ;([hasOneChild? x] relink x.parent to x.child)
+    ;([hasTwoChildren? x] delete and relink to smallest node in right tree)
+    ([not(= x (car tree))])))
   
 (trace-define (empty-tree? tree)
   (cond
@@ -43,8 +73,8 @@
 
 (define (bst-height tree)
   (cond
-    ([null? tree] 0)
-    ([empty? (car tree)] 0)
+    ([empty? (car tree)] -1)
+    ([and (empty? (left-subtree tree)) (empty? (right-subtree tree))] 0)
     (else (+ 1 (max (bst-height (left-subtree tree)) (bst-height (right-subtree tree)))))))
 
 (define (bst-size tree)
@@ -53,7 +83,43 @@
     ([empty? (car tree)] 0)
     (else (+ 1 (bst-size (left-subtree tree)) (bst-size (right-subtree tree))))))
 
+; tree definitions
 (define a (bst-insert null 5))
 (define b (bst-insert null null))
 (define c (bst-insert (bst-insert (bst-insert null 5) 8) 2))
 (define d (bst-insert (bst-insert (bst-insert (bst-insert (bst-insert null 5) 8) 2) 6) 7))
+
+; unit tests
+(check-equal? '(5 () ())
+              (bst-insert null 5))
+(check-equal? '(() () ())
+              (bst-insert null null))
+(check-equal? '(5
+                (2 () ()) (8 () ()))
+              (bst-insert (bst-insert (bst-insert null 5) 8) 2))
+(check-equal? '(5
+                (2 () ()) (8
+                       (6 ()
+                          (7 () ())) ()))
+              (bst-insert (bst-insert (bst-insert (bst-insert (bst-insert null 5) 8) 2) 6) 7))
+(check-equal? (empty-tree? b) #t "Empty tree should return true")
+(check-equal? (empty-tree? c) #f "Non-empty tree should return false")
+(check-equal? (bst-size a) 1 "One node size test")
+(check-equal? (bst-size b) 0 "Empty tree size test")
+(check-equal? (bst-size c) 3 "Three node size test")
+(check-equal? (isLeaf? a 5) #t "Root with empty tree true case")
+(check-equal? (isLeaf? a 10) #f "Root with empty tree false case")
+(check-equal? (isLeaf? b 5) #f "Completely empty tree")
+(check-equal? (isLeaf? c 5) #f "Tree with subtrees root should not be leaf")
+(check-equal? (isLeaf? c 8) #t "Is a leaf")
+(check-equal? (isLeaf? c 10) #f "Not a leaf")
+(check-equal? (isLeaf? d 5) #f "Not a leaf")
+(check-equal? (isLeaf? d 2) #t "Not a leaf")
+(check-equal? (isLeaf? d 8) #f "Not a leaf")
+(check-equal? (isLeaf? d 6) #f "Not a leaf")
+(check-equal? (isLeaf? d 7) #t "Is a leaf")
+
+(check-equal? (bst-height a) 0 "Height of a tree with only one node should be zero")
+(check-equal? (bst-height b) -1 "Height of a tree with zero nodes should be -1")
+(check-equal? (bst-height c) 1 "Height of node with two children should be 1")
+
