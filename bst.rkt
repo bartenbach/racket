dfcf7dfb8f51f799a678c240846acd0d91400f14
Blake Bartenbach <blakebartenbach@gmail.com>
2020-03-15 16:48:46 -0500

    Cleaned up code and wrote more unit tests for submission

--- bst.rkt
+++ bst.rkt
@@ -8,23 +8,15 @@
 ;; '(4 (3 () ()) (5 () ()))
 ;;  (root (left tree) (right tree))
 
-; returns the left subtree of a given tree
+; Returns the left subtree of a given tree
 (define (left-subtree tree)
   (car (cdr tree)))
 
-; returns the right subtree of a given tree
+; Returns the right subtree of a given tree
 (define (right-subtree tree)
   (car (cdr (cdr tree))))
 
-; returns the subtree of a tree starting with node 'x' and all children
-(define (get-subtree tree x)
-  (cond
-    ([and (null? (left-subtree tree)) (null? (right-subtree tree))] null)
-    ([= x (car (left-subtree tree))] x)
-    ([= x (car (right-subtree tree))] x)
-    (else (get-subtree (left-subtree tree) x) (get-subtree (right-subtree tree) x))))
-
-; Returns true if x in tree is a leaf - otherwise false
+; Returns true if x in the tree is a leaf - otherwise false
 (define (isLeaf? tree x)
   (cond
     ; the tree is empty
@@ -38,22 +30,25 @@
     ([> x (car tree)] (isLeaf? (right-subtree tree) x))
     (else #f)))
 
-; returns the largest node in a binary tree
+; Returns the largest node in a binary search tree.
 (define (max-node tree)
   (cond
     ([null? (right-subtree tree)] (car tree))
     (else (max-node (right-subtree tree)))))
 
+; Returns true iff the provided binary search tree (or subset) has only one child.
 (define (hasOneChild? x)
   (cond
     ([xor (null? (left-subtree x)) (null? (right-subtree x))] #t)
     (else #f)))
 
+; Returns true if the provided binary search tree (or subset) has two child nodes.
 (define (hasTwoChildren? x)
   (cond
     ([and (not(null? (left-subtree x))) (not(null? (right-subtree x)))] #t)
     (else #f)))
 
+; Provides an interface to create binary search tree objects and insert new nodes into existing binary search trees.
 (define (bst-insert tree x)
   (cond
     ([null? tree] (list x null null))
@@ -61,12 +56,14 @@
     ([> x (car tree)] (list (car tree) (car (cdr tree)) (bst-insert (car (cdr (cdr tree))) x)))
     (else tree)))
 
+; Provides the interface for deleting a node 'x' from a given binary search tree.
 (define (bst-delete tree x)
   (cond
     ([not(bst-contains? tree x)] tree) ; this is identical to Java's behavior with ArrayList.remove()
     ([and (empty? (left-subtree tree)) (empty? (right-subtree tree)) (= x (car tree))] '(() () ())) ; just a root node
     (else (bst-delete-recursive tree x))))
 
+; The 'private' function called by bst-delete that recursively calls itself to delete a node from a binary search tree.
 (define (bst-delete-recursive tree x)
   (cond
     ([null? tree] null)
@@ -76,35 +73,38 @@
     ([and (= x (car tree)) (hasOneChild? tree)] (relink-child tree x))
     ([and (= x (car tree)) (hasTwoChildren? tree)] (cons (relink-children tree x) null))))
 
+; Relinks a binary search tree with only one child without 'x'.
 (define (relink-child tree x)
   (cond
     ([null? (right-subtree tree)] (cons (left-subtree tree) null))
     (else (cons (right-subtree tree) null))))
 
+; Provides functionality to relink a binary search tree that has two children.
+; It does so by returning a binary search tree with the largest node in the right subtree replacing
+; the node we are deleting.  The left subtree is then linked as it was previously.  The right subtree
+; is linked again as the right subtree but minus the value we promoted to the new 'root' of the provided tree.
 (define (relink-children tree x)
     (cons (max-node (right-subtree tree))
-           (cons (left-subtree tree) (cons (bst-delete (right-subtree tree) (max-node (right-subtree tree))) null))))
+           (cons (left-subtree tree)
+                 (cons (bst-delete (right-subtree tree) (max-node (right-subtree tree))) null))))
 
+; Returns a completely empty binary search tree.
 (define (empty-bst)
   (cons null (cons null (cons null '()))))
 
+; Returns a binary search tree with a given leaf 'x' removed deleted.
 (define (leaf-deletion tree x)
   (cond
     ([< x (car tree)] (cons (car tree) (cons (empty-bst) (cdr (cdr tree)))))
     (else (cons (car tree) (cons (car (cdr tree)) (cons (empty-bst) null))))))
 
-(define (blank-node x tree)
- (cond
-  ((null? tree) '())
-  ((list? (car tree)) (cons (blank-node x (car tree)) (blank-node x (cdr tree))))
-  ([= x (car tree)] (cons '() (blank-node a (cdr tree))))
-  (else (cons (car tree) (blank-node x (cdr tree))))))
-  
+; Returns true if a given binary search tree is empty, otherwise false.
 (define (empty-tree? tree)
   (cond
     ([null? (car tree)] true)
     (else false)))
 
+; TODO - unfinished...would be nice though.
 (define (bst-print tree)
   (print "  ")
   (print (car tree))
@@ -113,6 +113,7 @@
   (print " ")
   (print (left-subtree tree)))
 
+; Returns true if provided tree contains value 'x', otherwise false
 (define (bst-contains? tree x)
   (cond
     ([empty? tree] #f)
@@ -121,12 +122,14 @@
     ([< x (car tree)] (bst-contains? (left-subtree tree) x))
     ([> x (car tree)] (bst-contains? (right-subtree tree) x))))
 
+; Returns the height of a given binary search tree.
 (define (bst-height tree)
   (cond
     ([empty? (car tree)] -1)
     ([and (empty? (left-subtree tree)) (empty? (right-subtree tree))] 0)
     (else (+ 1 (max (bst-height (left-subtree tree)) (bst-height (right-subtree tree)))))))
 
+; Returns the number of nodes in a given binary search tree.
 (define (bst-size tree)
   (cond
     ([null? tree] 0)
@@ -172,6 +175,10 @@
 (check-equal? (bst-delete c 2) '(5 (() () ()) (8 () ())) "Testing leaf deletion on tree c")
 (check-equal? (bst-delete d 8) '(5 (2 () ()) (6 () (7 () ()))) "Testing one child deletion on d")
 (check-equal? (bst-delete e 8) '(5 (2 () ()) (9 (6 () ()) (() () ()))) "Testing two child deletion on e")
+(check-equal? (bst-delete f 2) '(5 (() () ()) (9 (6 () (7 () ())) ())) "Testing one child deletion on f")
+(check-equal? (bst-delete f 9) '(5 (2 () ()) (6 () (7 () ()))) "Testing two child deletion on f")
+; TODO - this could use a more rigorous suite of test cases performing many different deletion scenarios
 (check-equal? (max-node a) 5 "Testing largest node in a tree is 5")
 (check-equal? (max-node c) 8 "Testing largest node in c tree is 8")
 (check-equal? (max-node e) 9 "Testing largest node in e tree is 9")
+(check-equal? (empty-bst) '(() () ()) "Testing empty binary search tree generates correctly")
